4191d40a7a72dd6eb4e73f377d5ae6fd521d17ff
Svjetlana Zolja <svjetlanazolja@gmail.com>
2022-01-11 12:33:35 +0100

    add components

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from "./main.module.css";
 import Button from "../Button/buttonPhone";
-import Contact from "../Contact/contact";
+import Modal from "../Modal/modal";
 import Table from "../Table/table";
 
 const Main = () => {
   return (
     <div className={styles.Main}>
       <p>test main</p>
+      <Button />
+      <Table />
+      <Modal />
     </div>
   );
 };
---
+++ src/components/Modal/modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./modal.module.css";
+
+const Modal = () => {
+  return (
+    <div className={styles.modal}>
+      <p>test modal</p>
+    </div>
+  );
+};
+
+export default Modal;
